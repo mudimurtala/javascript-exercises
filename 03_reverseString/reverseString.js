96f78caac4a0ab2fddfd040f6366bde1a17204fc
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-06 17:05:08 +0100

    Add solutions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let result1 = '';
+    let result2 = '';
+    result1 = word.split("");
+    result2 = result1.reverse().join("");
+    return result2;
 };
 
 // Do not edit below this line
