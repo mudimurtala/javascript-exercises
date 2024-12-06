3aedbeeb4f08023905ccc6830b6fe013b7204e7c
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-06 11:26:40 +0100

    Add solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -3,7 +3,9 @@ const repeatString = function(string, num) {
     if (num < 0) {
         return 'ERROR';
     }
-    
+
+    num = Math.random() * 1000;
+
     for (let i = 0; i < num; i++) {
         result += string;
     }
