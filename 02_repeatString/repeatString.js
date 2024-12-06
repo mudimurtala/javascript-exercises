45f9a35171af436624e5ee9ebfada6d23fdf4051
mudimurtala <ibnmuhyideen95@gmail.com>
2024-12-06 11:19:35 +0100

    Add solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
 const repeatString = function(string, num) {
     let result = '';
+    if (num < 0) {
+        return 'ERROR';
+    }
+    
     for (let i = 0; i < num; i++) {
         result += string;
     }
